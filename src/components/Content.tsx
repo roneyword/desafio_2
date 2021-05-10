66504bb9563542886fc4f859bb2680da7d0e993d
Roney Berti <33360638+roneyword@users.noreply.github.com>
2021-05-10 00:18:16 -0300

    refatoração das funcionalidades content e sidebar

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,43 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from './MovieCard';
+import { Header } from './Header'
+
+import '../styles/content.scss';
+
+interface ContentProps {
+  selectedGenre: { title: string };
+  movies: Array<{
+    Title: string;
+    Poster: string;
+    Runtime: string;
+    Ratings: Array<{ Value: string }>;
+  }>;
+}
+
+export function Content({ movies, selectedGenre }: ContentProps) {
+
+  return (
+    <div className="container">
+      <Header title={selectedGenre.title}/>
+      {/* <header>
+        <span className="category">
+          Categoria:
+          <span> {selectedGenre.title}</span>
+        </span>
+      </header> */}
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard
+              key={movie.Title}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
